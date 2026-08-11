7f199d907a30f23cd95bbd849350b5f008c8b58e
DavidJay109 <olumidejay5@gmail.com>
2026-08-11 11:37:24 -0400

    added profile page and plant-catalog page

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: "localhost",       // where MySQL is running
   port: 3306,
   user: "root",             // your MySQL username
-  password: "olumide980",              // your MySQL password
+  password: "",              // your MySQL password
   database: "sproutly_app_db",     // the database we created above
 });
 
